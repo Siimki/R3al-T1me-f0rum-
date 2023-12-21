a1deee07e53ae6aac6b38763cf0a0c9c56f6571b
Roman <romanmarkilov@gmail.com>
2023-12-21 22:54:18 +0200

    fixed logic and cleared yellow errors

--- static/displayComments.js
+++ static/displayComments.js
@@ -1,34 +1,16 @@
 export function displayComments() {
-    // Your code for setting up comment toggling
-    document.addEventListener('click', function(event) {
-        // Check if the clicked element has the .comments-toggler class
-        if (event.target.classList.contains('comments-toggler')) {
-            console.log("Here i display comment")
-            const postId = event.target.dataset.postId;
-            const commentsDiv = document.getElementById(`comments${postId}`);
-            if (commentsDiv) {
-                console.log("Here i really display comment")
-                commentsDiv.style.display = commentsDiv.style.display === 'none' ? 'block' : 'none';
-            }
-        }
-    });
+  // Your code for setting up comment toggling
+  document.addEventListener("click", function (event) {
+    // Check if the clicked element has the .comments-toggler class
+    if (event.target.classList.contains("comments-toggler")) {
+      console.log("Here i display comment");
+      const postId = event.target.dataset.postId;
+      const commentsDiv = document.getElementById(`comments${postId}`);
+      if (commentsDiv) {
+        console.log("Here i really display comment");
+        commentsDiv.style.display =
+          commentsDiv.style.display === "none" ? "block" : "none";
+      }
+    }
+  });
 }
-
-
-// export function displayComments() {
-//     document.addEventListener('DOMContentLoaded', (event) => {
-//         console.log("Do i come to the displaycomments.js?")
-    
-//         document.querySelectorAll('.comments-toggler').forEach(item => {
-//             item.addEventListener('click', event => {
-//                 const postId = event.target.getAttribute('data-post-id');
-//                 const commentsDiv = document.querySelector(`#comments${postId}`);
-//                 if (commentsDiv.style.display === "none") {
-//                     commentsDiv.style.display = "block";
-//                 } else {
-//                     commentsDiv.style.display = "none";
-//                 }
-//             })
-//         })
-//     });
-// }
